88a8ac3c7f5217d1b77bdb809ba9e3673bd6ea76
user-5005 <2022100187t@psu.palawan.edu.ph>
2025-05-26 00:16:59 +0800

    Add files via upload

---
+++ data/poblacion_transport_4.js
@@ -0,0 +1,8 @@
+var json_poblacion_transport_4 = {
+"type": "FeatureCollection",
+"name": "poblacion_transport_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "2146206123", "code": 5622.0, "fclass": "bus_station", "name": "Taytay Transport Terminal" }, "geometry": { "type": "Point", "coordinates": [ 119.4993366, 10.8175501 ] } }
+]
+}
---
+++ data/poblacion_waterways_0.js
@@ -0,0 +1,13 @@
+var json_poblacion_waterways_0 = {
+"type": "FeatureCollection",
+"name": "poblacion_waterways_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "609536557", "code": 8101.0, "fclass": "river", "width": 0.0, "name": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.470729876079972, 10.820137335781235 ], [ 119.4708984, 10.8203231 ], [ 119.471194, 10.820309 ], [ 119.4714658, 10.8203793 ], [ 119.4718472, 10.820309 ], [ 119.4722048, 10.8201029 ], [ 119.4723717, 10.8199016 ], [ 119.4723431, 10.8196674 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "1338216550", "code": 8102.0, "fclass": "stream", "width": 0.0, "name": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.5087905, 10.8251721 ], [ 119.5089045, 10.8255515 ], [ 119.5089769, 10.8256543 ], [ 119.5090185, 10.8257833 ], [ 119.5091808, 10.8258439 ], [ 119.5092358, 10.8259151 ], [ 119.5092639, 10.8259809 ], [ 119.5092317, 10.8260679 ], [ 119.5090259, 10.8261765 ], [ 119.5086993, 10.8263814 ], [ 119.5086269, 10.8266501 ], [ 119.5083936, 10.8267673 ], [ 119.508175, 10.8267568 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "1338216578", "code": 8104.0, "fclass": "drain", "width": 0.0, "name": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.5161827, 10.8256161 ], [ 119.5161706, 10.8256681 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "1338216579", "code": 8104.0, "fclass": "drain", "width": 0.0, "name": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.5161961, 10.8252031 ], [ 119.5161827, 10.8256161 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "1338216584", "code": 8104.0, "fclass": "drain", "width": 0.0, "name": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.5151882, 10.8262225 ], [ 119.515172123942222, 10.826222946557161 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "1338216585", "code": 8104.0, "fclass": "drain", "width": 0.0, "name": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.5161706, 10.8256681 ], [ 119.5161009, 10.8257893 ], [ 119.5160861, 10.8259533 ], [ 119.516058, 10.8261535 ], [ 119.5160211, 10.8261983 ], [ 119.5151882, 10.8262225 ] ] ] } }
+]
+}
